35c26f6c068fdb8676b2b469697420ba387f4386
gregorionunez <colorc98@gmail.com>
2023-08-07 20:36:16 -0300

    Initial commit

---
+++ src/main/java/ejercicio2github/Ejercicio2GitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicio2github;
+
+/**
+ *
+ * @author Gregorio
+ */
+public class Ejercicio2GitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
